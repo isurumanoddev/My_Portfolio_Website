ddc700deda06702a5c8d9387bdef75f9b97b8c2e
isuru <isurumanod99@gmail.com>
2023-03-19 10:43:03 +0530

    ui update

--- my_website/urls.py
+++ my_website/urls.py
@@ -6,5 +6,7 @@ from my_website import views
 urlpatterns = [
     path('', views.home,name="home"),
     path('projects/', views.projects,name="projects"),
+    path('about/', views.about,name="about"),
+    path('contacts/', views.contact,name="contacts"),
 
 ]
--- my_website/views.py
+++ my_website/views.py
@@ -9,3 +9,13 @@ def home(request):
 def projects(request):
     context = {}
     return render(request, "projects.html", context)
+
+def about(request):
+    context = {}
+    return render(request, "about.html", context)
+
+
+def contact(request):
+    context = {}
+    return render(request, "contact.html", context)
+
