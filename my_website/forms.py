76e9d99a1e24f5c2af33ef073b656acdeb671a5f
isuru <isurumanod99@gmail.com>
2023-03-20 13:34:19 +0530

    ui update

--- my_website/forms.py
+++ my_website/forms.py
@@ -5,5 +5,11 @@ from django.forms import ModelForm
 class ProjectForm(ModelForm):
     class Meta:
         model = Projects
-        fields = ["title","thumbnail","body"]
+        fields = ["title","body"]
 
+    def __init__(self, *args, **kwargs):
+        super(ProjectForm,self).__init__(*args,**kwargs)
+
+        self.fields["title"].widget.attrs.update({'class':'form-control'})
+        self.fields["body"].widget.attrs.update({'class':'form-control'})
+        # self.fields["thumbnail"].widget.attrs.update({'class':'form-control'})
